da589f0b274e9e3041584b0cbce1b154d4c5138c
anjalish2303-glitch <anjalish2303@gmail.com>
2025-11-14 10:43:46 +0530

    Add files via upload

---
+++ bca_python_assignment1.py
@@ -0,0 +1,9 @@
+#Task 1
+# Name:Anjali Sharma
+# Roll No: 2501060103
+# Course: BCA
+# Semester: 1st
+# Subject: Problem Solving with Python
+# Assignment: Unit-1 Mini Project
+# Title: Student Profile Console App
+# Date:13,november 2025
